15a40cf84104fa5f34ef94c1f3891856a45bc581
Dominic Hall <dh486@molerat.bio.cam.ac.uk>
2021-08-26 15:58:09 +0100

    autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,3 +58,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Dependency on C-extension modules break ReadTheDocs without the mock import.
+autodoc_mock_imports = ["GrapHiC.utils.dtrack_utils"]
