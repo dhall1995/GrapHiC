453af28135e30e42129724a4688bf6d0bebcf1a2
Dominic Hall <dh486@molerat.bio.cam.ac.uk>
2021-08-27 11:25:29 +0100

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,4 +60,4 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # Dependency on C-extension modules break ReadTheDocs without the mock import.
-autodoc_mock_imports = ["numpy", "cython", "pandas", "networkx", "cooler", "Cython","GrapHiC.utils.dtrack_utils"]
+autodoc_mock_imports = ["GrapHiC.utils.dtrack_utils"]
